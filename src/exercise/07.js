30bd56e822a80c65d9a8fed8d5917717b4907015
Antoniel Magalhães <antoniel2210@gmail.com>
2021-01-18 14:24:23 -0300

    Finish react-fundamentals course

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -32,6 +32,7 @@ function App() {
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
         {items.map(item => (
           // 🐨✅ add a key prop to the <li> below. Set it to item.id
+          // Finish Coures
           <li key={item.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
